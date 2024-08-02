3f7b8bec656eb65d30e45b130f7e78ed0a8bfa93
Rogelio Olarte <galaxino12@gmail.com>
2024-08-02 14:54:43 -0500

    Update routes_api.tsx

--- frontend/src/config/routes_api.tsx
+++ frontend/src/config/routes_api.tsx
@@ -1,5 +1,5 @@
-export const MAIN_API = 'http://localhost:3000'
-/* export const MAIN_API = 'https://orderly-api.onrender.com/' */
+/* export const MAIN_API = 'http://localhost:3000' */
+export const MAIN_API = 'https://orderly-api.onrender.com/'
 export const MAIN_SOCKET = 'ws://orderly-api.onrender.com/'
 export const ROUTE_LOGIN = ''
 export const ROUTE_MENU = '/api/menu/all'
